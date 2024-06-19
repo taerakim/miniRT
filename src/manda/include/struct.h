669e8c5c7c9fdb44496e3146924fa34a0e5286e1
YeonDeok <yeondeok99@naver.com>
2024-06-19 14:32:50 +0900

    Feat: 구조체 추가 (#5)
    init: parsing structure

---
+++ src/manda/include/struct.h
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeondcho <yeondcho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/19 14:07:28 by yeondcho          #+#    #+#             */
+/*   Updated: 2024/06/19 14:22:37 by yeondcho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCT_H
+# define STRUCT_H
+
+typedef struct s_vec	t_vec;
+typedef struct s_vec	t_point;
+
+struct s_vec
+{
+	double	x;
+	double	y;
+	double	z;
+};
+
+typedef struct s_color
+{
+	double	r;
+	double	g;
+	double	b;
+}	t_color;
+
+typedef enum e_obj_type
+{
+	sphere,
+	plane,
+	cylinder
+}	t_obj_type;
+
+typedef struct s_object
+{
+	t_obj_type	type;
+	t_point		pos;
+	t_vec		nvec;
+	t_color		rgb;
+	double		ratio;
+	double		diameter;
+	double		height;
+	void		*next;
+}	t_object;
+
+typedef struct s_camera
+{
+	t_point	pos;
+	t_vec	nvec;
+	int		fov;
+}	t_camera;
+
+typedef struct s_ambient
+{
+	t_color	rgb;
+	double	ratio;
+}	t_ambient;
+
+typedef struct s_light
+{
+	t_point	pos;
+	double	ratio;
+}	t_light;
+
+typedef struct s_element
+{
+	t_camera	camera;
+	t_ambient	ambient;
+	t_light		light;
+	t_object	*objs;
+}	t_element;
+
+#endif
