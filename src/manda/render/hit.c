22edc48a7d9933af3ae37df829f6bfbb2179bd71
yeondcho <yeondcho@student.42seoul.kr>
2024-06-24 19:47:20 +0900

    feat: 평면, 구 hit_record 업데이트 및 판별식 수정

--- src/manda/render/hit.c
+++ src/manda/render/hit.c
@@ -6,7 +6,7 @@
 /*   By: yeondcho <yeondcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/21 15:29:47 by taerakim          #+#    #+#             */
-/*   Updated: 2024/06/23 22:31:04 by yeondcho         ###   ########.fr       */
+/*   Updated: 2024/06/24 19:40:17 by yeondcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,112 +22,44 @@ void	_check_front(t_vec ray, t_hit *record)
 	else
 	{
 		record->isfront = false;
-		record->nvec = vmulti_s(record->nvec, -1);
+		record->nvec = vunit(vmulti_s(record->nvec, -1));
 	}
-}
-
-bool	hit_cylinder(t_camera *camera, t_object *cylinder, t_vec ray)
-{
-	if (hit_cylinder_side(camera, cylinder, ray))
-	{
-		return (true);
-	}
-	else
-	{
-		if (hit_cylinder_base(camera, cylinder, ray, -1) \
-		|| hit_cylinder_base(camera, cylinder, ray, 1))
-		{
-			return (true);
-		}
-	}
-	return (false);
-}
-
-double	min(double a, double b)
-{
-	if (a > 0 && b > 0)
-	{
-		if (a > b)
-			return (b);
-		else
-			return (a);
-	}
-	else if (a > 0)
-		return (a);
-	else if (b > 0)
-		return (b);
-	return (0);
-}
-
-bool	hit_cylinder_side(t_camera *camera, t_object *cylinder, t_vec ray)
-{
-	const t_vec		ce = vminus(camera->point \
-							, vplus(vmulti_s(cylinder->nvec \
-							, -1 * (cylinder->height / 2)), cylinder->point));
-	const double	a = pow(vinner(ray, cylinder->nvec), 2) - 1;
-	const double	b = vinner(ray, cylinder->nvec) \
-						* vinner(ce, cylinder->nvec) - vinner(ce, ray);
-	const double	c = pow(cylinder->diameter / 2, 2) - vinner(ce, ce) \
-						+ pow(vinner(ce, cylinder->nvec), 2);
-	const double	det = b * b - a * c;
-	const double	t = min((-b + sqrt(det)) / a, (-b - sqrt(det)) / a);
-	const t_vec		cp = vplus(ce, vmulti_s(ray, t));
-
-	if (det < 0 || t < 0)
-		return (false);
-	if (!(0 <= vinner(cp, cylinder->nvec) && vinner(cp, cylinder->nvec) <= cylinder->height))
-		return (false);
-	return (true);
-}
-
-bool	hit_cylinder_base(t_camera *camera, t_object *cylinder, \
-t_vec ray, int dir)
-{
-	const t_vec		c = vplus(vmulti_s(cylinder->nvec, dir * (cylinder->height / 2)), cylinder->point);
-	const t_vec		ec = vminus(c, camera->point);
-	const double	t = vinner(ec, cylinder->nvec) / vinner(ray, cylinder->nvec);
-	const t_vec		cp = vplus(vminus(camera->point, c), vmulti_s(ray, t));
-
-	if (vinner(cp, cp) <= pow(cylinder->diameter / 2, 2))
-		return (true);
-	return (false);
 }
 
 bool	hit_sphere(t_camera *camera, t_object *sphere, t_vec ray, t_hit *record)
 {
-	const double	a = 1;
-	const double	b = vinner(ray, vminus(camera->point, sphere->point));
-	const double	c = vinner(vminus(camera->point, sphere->point) \
-								, vminus(camera->point, sphere->point)) \
-						- pow(sphere->diameter / 2, 2);
-	const double	det = b * b - a * c;
-	double			sqrted;
-	double			root;
+	const t_det	ans = det(1, vinner(ray, vminus(camera->point, sphere->point)), \
+	vinner(vminus(camera->point, sphere->point), \
+	vminus(camera->point, sphere->point)) - pow(sphere->diameter / 2, 2));
 
-	if (det < 0)
+	if (ans.det < 0)
 		return (false);
-	sqrted = sqrt(det);
-	root = (sqrted - b) / a;
-	if (root < record->tmin || record->tmax < root)
-	{
-		root = (sqrted - b) / a;
-		if (root < record->tmin || record->tmax < root)
-			return (false);
-	}
-	record->t = root;
-	record->p = vplus(camera->point, vmulti_s(ray, root));
-	record->nvec = vdivi_s(vminus(record->p, sphere->point)\
-								, sphere->diameter / 2);
-	
+	if (ans.t < record->tmin || record->tmax < ans.t)
+		return (false);
+	record->t = ans.t;
+	record->tmax = ans.t;
+	record->p = vplus(camera->point, vmulti_s(ray, ans.t));
+	record->nvec = vunit(vdivi_s(vminus(record->p, sphere->point) \
+								, sphere->diameter / 2));
 	_check_front(ray, record);
 	return (true);
 }
 
-bool	hit_plane(t_object *plane, t_vec ray)
+bool	hit_plane(t_camera *camera, t_object *plane, t_vec ray, t_hit *record)
 {
-	if (vinner(ray, plane->nvec) < 0)
-		return (true);
-	return (false);
+	const t_vec	ea = vminus(plane->point, camera->point);
+	double		t;
+
+	if (vinner(ray, plane->nvec) == 0)
+		return (false);
+	t = vinner(ea, plane->nvec) / vinner(ray, plane->nvec);
+	if (t < record->tmin || record->tmax < t)
+		return (false);
+	record->t = t;
+	record->tmax = t;
+	record->p = vplus(camera->point, vmulti_s(ray, t));
+	record->nvec = plane->nvec;
+	return (true);
 }
 
 bool	hit_object(t_camera *camera, t_object *objs, t_vec ray, t_hit *record)
@@ -135,8 +67,8 @@ bool	hit_object(t_camera *camera, t_object *objs, t_vec ray, t_hit *record)
 	if (objs->type == type_sphere)
 		return (hit_sphere(camera, objs, ray, record));
 	if (objs->type == type_plane)
-		return (hit_plane(objs, ray));
+		return (hit_plane(camera, objs, ray, record));
 	if (objs->type == type_cylinder)
-		return (hit_cylinder(camera, objs, ray));
+		return (hit_cylinder(camera, objs, ray, record));
 	return (false);
 }
