1b4357cf16b5010c001dd84e5698b3f1a5ce8bb2
Taeran <135775997+taerakim@users.noreply.github.com>
2024-06-24 20:56:52 +0900

    feat: 난반사 구현OC (#16)

--- src/manda/include/rt_struct.h
+++ src/manda/include/rt_struct.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rt_struct.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yeondcho <yeondcho@student.42.fr>          +#+  +:+       +#+        */
+/*   By: taerakim <taerakim@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 14:07:28 by yeondcho          #+#    #+#             */
-/*   Updated: 2024/06/24 17:07:11 by yeondcho         ###   ########.fr       */
+/*   Updated: 2024/06/24 19:54:30 by taerakim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,6 +66,7 @@ typedef struct s_light
 {
 	t_point	point;
 	double	ratio;
+	t_color	rgb;// parsing 된다 했었던 것 같은데
 }	t_light;
 
 typedef struct s_element
@@ -78,6 +79,7 @@ typedef struct s_element
 
 typedef struct s_hit
 {
+	bool	ishit;
 	t_vec	nvec;
 	t_point	p;
 	double	t;
