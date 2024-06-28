c55a434fbaccf4999c95cd9a60adddd34a06bf46
yeondcho <yeondcho@student.42seoul.kr>
2024-06-28 13:11:17 +0900

    feat: 파싱 예외처리, 널가드 및 메모리 할당 추가

--- src/manda/parse/parse.c
+++ src/manda/parse/parse.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taerakim <taerakim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: yeondcho <yeondcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 22:18:30 by yeondcho          #+#    #+#             */
-/*   Updated: 2024/06/23 17:21:55 by taerakim         ###   ########.fr       */
+/*   Updated: 2024/06/27 22:11:09 by yeondcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,12 +34,17 @@ void	create_object(t_object **head, char **vals)
 	t_object	*ptr;
 	t_object	*new;
 
+	new = NULL;
 	if (ft_strncmp(vals[0], "pl", 3) == 0)
 		new = create_plane(&vals[1]);
 	if (ft_strncmp(vals[0], "sp", 3) == 0)
 		new = create_sphere(&vals[1]);
 	if (ft_strncmp(vals[0], "cy", 3) == 0)
 		new = create_cylinder(&vals[1]);
+	if (ft_strncmp(vals[0], "co", 3) == 0)
+		new = create_cone(&vals[1]);
+	if (!new)
+		return ;
 	ptr = *head;
 	if (ptr == NULL)
 	{
@@ -58,14 +63,20 @@ void	parse_to_obj(t_element *element, char *line)
 	obj_args = rt_split(line);
 	if (obj_args == NULL)
 		return ;
+	if (ft_strncmp(obj_args[0], "#", 2) == 0)
+	{
+		ft_clear_char(obj_args);
+		return ;
+	}
 	if (ft_strncmp(obj_args[0], "A", 2) == 0)
 		element->ambient = create_ambient(&obj_args[1]);
 	else if (ft_strncmp(obj_args[0], "L", 2) == 0)
-		element->light = create_light(&obj_args[1]);
+		create_light(&element->light, &obj_args[1]);
 	else if (ft_strncmp(obj_args[0], "C", 2) == 0)
 		element->camera = create_camera(&obj_args[1]);
 	else
 		create_object(&element->objs, obj_args);
+	ft_clear_char(obj_args);
 }
 
 t_element	ft_parse_rt(int fd)
@@ -74,6 +85,7 @@ t_element	ft_parse_rt(int fd)
 	char		*line;
 
 	obj.objs = NULL;
+	obj.light = NULL;
 	line = get_next_line(fd);
 	while (line)
 	{
--- src/manda/parse/parse.h
+++ src/manda/parse/parse.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parse.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: taerakim <taerakim@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: yeondcho <yeondcho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/19 15:36:16 by yeondcho          #+#    #+#             */
-/*   Updated: 2024/06/23 17:21:40 by taerakim         ###   ########.fr       */
+/*   Updated: 2024/06/27 21:58:24 by yeondcho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,16 @@ t_element	ft_parse_rt(int fd);
 t_vec		vec(char *vec);
 t_point		point(char *pos);
 t_color		color(char *rgb);
-t_ambient	create_ambient(char **vals);
-t_camera	create_camera(char **vals);
-t_light		create_light(char **vals);
+int			arg_len(char **vals);
+/* constructor.c*/
 t_object	*create_cylinder(char **vals);
 t_object	*create_plane(char **vals);
 t_object	*create_sphere(char **vals);
+t_object	*create_cone(char **vals);
+void		create_light(t_light **head, char **vals);
+/* constructor2.c*/
+t_ambient	create_ambient(char **vals);
+t_camera	create_camera(char **vals);
 
 t_element	ft_parse_rt(int fd);
 char		**rt_split(char *str);
@@ -38,7 +42,5 @@ void		print_element(t_element *elem);
 void		add_light_to_list(t_light **head, t_light *new);
 void		ft_clear_char(char **str);
 int			atod(char *str, double *result);
-void		print_element(t_element *elem);
-int			arg_len(char **vals);
 
 #endif
