58df9a4b285e1fbeb8232a0c5e13de7fc704eb1c
yeondcho <yeondcho@student.42seoul.kr>
2024-06-28 13:15:08 +0900

    feat: 광원추적 로직 추가

---
+++ src/manda/render/hit_trace.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   hit_trace.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yeondcho <yeondcho@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 15:47:29 by yeondcho          #+#    #+#             */
+/*   Updated: 2024/06/27 19:56:26 by yeondcho         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <math.h>
+#include "rt_struct.h"
+#include "vector.h"
+#include "minirt.h"
+
+bool	hit_trace(t_object **head, t_hit *hit, t_light *light)
+{
+	t_hit		record;
+	t_vec		lp;
+	t_vec		ray;
+
+	hit->p = vplus(hit->p, vmulti_s(hit->nvec, BIAS));
+	lp = vminus(hit->p, light->point);
+	record.tmin = 1e-6;
+	record.tmax = RENDER_MAX;
+	record.t = 0;
+	record.ishit = false;
+	ray = vunit(lp);
+	hit_object(light->point, head, ray, &record);
+	if (record.ishit && pow(record.tmax, 2) < vinner(lp, lp))
+		return (false);
+	return (record.ishit);
+}
